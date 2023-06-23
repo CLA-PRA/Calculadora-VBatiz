99f5634c58a069a927efd9dcf126eca5a76f0bfb
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2023-06-23 18:11:31 +0000

    Lista la calculadora para la tarea de los alumnos

--- miPrincipal/Calculadora.java
+++ miPrincipal/Calculadora.java
@@ -6,15 +6,5 @@ public class Calculadora {
         return x + y;
     }
     
-    public int resta(int x, int y) {
-        return x-y;
-    }
 
-    public int multiplica(int x, int y) {
-        return x * y;
-    }
-
-    public int divide(int x, int y) {
-        return x / y;
-    }
 }
